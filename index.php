2346d2b3856fc862af42a4c0e18a1aab67a67ee8
sikB <sikhovski@icloud.com>
2016-05-16 17:22:16 -0400

    Giant Manta Ray site
    site built on the LAMP stack.

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<!-- <script type="text/javascript" src='js'></script> -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
+</head>
+<body>
+	<div class="container col-sm-12">
+		<div class="row">
+			<nav class="navbar navbar-inverse daNav fixed-top">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <a class="navbar-brand" href="#">GMR</a>
+			    </div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="#">Home</a></li>
+			    </ul>
+			    <ul class="nav navbar-nav navbar-right">
+			      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+			      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			    </ul>
+			  </div>
+			</nav>
+		</div>
+		<div class="background">
+			<div class="row">
+				<img src="img/gmrBackground.jpeg">
+			</div>
+		</div>
+		<div class="mission">
+			<div class="row color">
+				<h1 class="text-center">My Mission..</h1>
+				<div class="missionText">
+					<h2>My mission is to stop this:</h2>
+					<img src="img/mantaHunters.png">
+					<h4 class="pStuff threats">Threats to GMRs</h4>
+					<p class="col-sm-5 pStuff">Just like all wild animals mantas, have a number of natural predators. Certain species of sharks, as well as orcas and false killer whales have all been recorded predating upon manta rays. Shark bites are seen within every population of manta rays that are studied. Often these bites are taken from the trailing edges of the manta's wings and in many cases these heal up quite well and leave the manta with no lasting damage.<br>
+
+In addition to natural predation, mantas face significant threats from humans and our activities. Fishing line can cause severe entanglement resulting in deep, and often life threatening lacerations if mantas become entangled in it. Gill nets and other fishing nets also cause mantas to become entangled, usually resulting in death. Unfortunately, mantas cannot swim backwards and so attempts to disentangle themselves often result in greater entrapment. Mantas which become entangled in nets quickly die because they are unable to actively pump oxygen rich water over their gills in order to respire.<br>
+
+	<em><strong>Most recently mantas rays have become a desirable catch in a number of countries around the world. The reason for this increase in demand is the manta ray's gill plates which have become highly desirable in the Chinese medicine trade. The manta's gill plates are cartilaginous finger-like projections that branch off the gill arch and are used by the mantas to filter their planktonic prey from the water. The trade in these plates has increased significantly in recent years, posing a worrying threat to manta and mobula populations around the world.</strong></em></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
